7cd06f77f8f3e5883ef1308fc3bd745141418b70
chenghz98 <2313902108@qq.com>
2021-03-19 21:27:49 +0800

    02_左侧导航栏样式

---
+++ src/permission.js
@@ -0,0 +1,27 @@
+import router from '@/router'
+import store from '@/store'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+const whiteList = ['/login', '/404']
+// 路由前置守卫
+router.beforeEach(function(to, from, next) {
+  NProgress.start()
+  if (store.getters.token) {
+    if (to.path === '/login') {
+      next('/')
+    } else {
+      next()
+    }
+  } else {
+    if (whiteList.indexOf(to.path) > -1) {
+      next()
+    } else {
+      next('/login')
+    }
+  }
+  NProgress.done()
+})
+// 路由后置守卫
+router.afterEach(function() {
+  NProgress.done()
+})
