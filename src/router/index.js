4c22038cc2912042b6c06b93717a47b643f30748
chenghz98 <2313902108@qq.com>
2021-03-21 19:50:09 +0800

    路由规则配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import approvalsRouter from './modules/approvals'
+import departmentsRouter from './modules/departments'
+import employeesRouter from './modules/employees'
+import permissionRouter from './modules/permission'
+import attendancesRouter from './modules/attendances'
+import salarysRouter from './modules/salarys'
+import settingRouter from './modules/setting'
+import socialRouter from './modules/social'
 Vue.use(Router)
 
 /* Layout */
 import Layout from '@/layout'
+// 动态路由
+export const asyncRoutes = [
+  approvalsRouter,
+  departmentsRouter,
+  employeesRouter,
+  permissionRouter,
+  attendancesRouter,
+  salarysRouter,
+  settingRouter,
+  socialRouter
+]
 export const constantRoutes = [
   {
     path: '/login',
@@ -26,7 +44,7 @@ export const constantRoutes = [
       path: 'dashboard',
       name: 'Dashboard',
       component: () => import('@/views/dashboard/index'),
-      meta: { title: 'Dashboard', icon: 'dashboard' }
+      meta: { title: '首页', icon: 'dashboard' }
     }]
   },
 
@@ -37,7 +55,7 @@ export const constantRoutes = [
 const createRouter = () => new Router({
   // mode: 'history', // require service support
   scrollBehavior: () => ({ y: 0 }),
-  routes: constantRoutes
+  routes: [...constantRoutes, ...asyncRoutes]
 })
 
 const router = createRouter()
--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,7 +1,30 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <span>组织架构</span>
+      <el-card>
+        <el-row type="flex" justify="space-between">
+          <el-col><span>江苏传智播客学院有限公司</span></el-col>
+          <el-col :span="4">
+            <el-row type="flex" justify="space-between">
+              <el-col><span>负责人</span></el-col>
+              <el-col>
+                <el-dropdown>
+                  <span>
+                    操作
+                    <i class="el-icon-arrow-down" />
+                  </span>
+                  <!-- 具名插槽 -->
+                  <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item>添加子部门</el-dropdown-item>
+                    <el-dropdown-item>删除部门</el-dropdown-item>
+                    <el-dropdown-item>修改部门</el-dropdown-item>
+                  </el-dropdown-menu>
+                </el-dropdown>
+              </el-col>
+            </el-row>
+          </el-col>
+        </el-row>
+      </el-card>
     </div>
   </div>
 </template>
